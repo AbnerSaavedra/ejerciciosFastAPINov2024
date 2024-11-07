272fa715654c015a30c571cb8836a291c8e77803
Abner Saavedra <ingenieroabnersaavedra@bitbucket.org>
2024-11-07 13:50:04 -0400

    Ejercicio 1 validación email con EmailStr

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -1,32 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from pydantic import field_validator
 from email_validator import validate_email, EmailNotValidError
+from pydantic import EmailStr
 
 
 class User(BaseModel):
     name: str
     age: int
-    email: str
+    email: EmailStr
     password: str
 
     # Creamos nuestras validaciones propias
     @field_validator("name")
-    def validate_name(name):
-        if len(name) == 0:
+    def validate_name(n):
+        if len(n) == 0:
             raise ValueError("Name should'nt be empty.")
-        return name
+        return n
     @field_validator("age")
-    def validate_age(age):
-        if age < 18 or age > 100:
+    def validate_age(a):
+        if a < 18 or a > 100:
             raise ValueError("Age not allowed")
-        return age
-    @field_validator("email")
+        return a
+    '''@field_validator("email")
     def email_validation(cls, email):
         try:
             v = validate_email(email)
             email = v['email']
         except EmailNotValidError as e:
-            raise ValueError('Email no permitido'. str(e))
+            raise ValueError('Email no permitido'. str(e))'''
     @field_validator("password")
     def validate_password(password):
         if len(password) < 5:
@@ -34,8 +35,10 @@ class User(BaseModel):
         return password
 
   
-user = User(name="Abner", age=33, email="abnersaavedra@gmail.com", password="123456")
-
-print("User: ", user)
+try:
+    user = User(name="", age=120, email="abnersaavedra@gmail.com", password="1234")
+    print("User: ", user)
+except ValidationError as e:
+    print(e.json())
 
 
